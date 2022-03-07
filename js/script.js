36fe55d04ab3231f193b4dd56e442015fd914b4e
cif95 <vivianacifarelli9@gmail.com>
2022-03-07 15:05:02 +0100

    refactoring

--- js/script.js
+++ js/script.js
@@ -4,49 +4,70 @@
 // con difficoltà 2 => tra 1 e 81
 // con difficoltà 3 => tra 1 e 49
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
-
-const levelSelectElement = document.getElementById('game-level');
-const playBtn = document.querySelector('#play-btn');
 const grid = document.getElementById('grid');
+const scoreOutput = document.getElementById('score');
+const levelSelectElement = document.getElementById('game-level');
 
 
-playBtn.addEventListener('click', function() {
+document.querySelector('#play-btn').addEventListener('click', function() {
 	grid.innerHTML = '';
-	switch (levelSelectElement.value) {
-		case 'easy' :
-			for ( let i = 1; i <= 100; i++) {
-				const gridBox = createGridBoxElement(i);
-				grid.appendChild(gridBox);
-			}
-		break
-		case 'intermediate' :
-			for ( let i = 1; i <= 81; i++) {
-				const gridBox = createGridBoxElement(i);
-				grid.appendChild(gridBox);
-			}
-		break
-		case 'difficult' :
-			for ( let i = 1; i <= 49; i++) {
-				const gridBox = createGridBoxElement(i);
-				grid.appendChild(gridBox);
-			}
-		break
-		default :
-		console.log('No difficulty level has been selected');
-	}
+	scoreOutput.innerHTML = '';
+	generateGame();
 })
 
+document.getElementById('reset-btn').addEventListener('click', function() {
+	grid.innerHTML = '';
+	scoreOutput.innerHTML = '';
+	levelSelectElement.value = "Choose a difficulty level";
+})
+
+
+
+
+// ! FUNCTIONS below ! //
 /**
- * function that creates a div grid box as a dom element, that becomes blue when clicked and has an inner html content
- * @param {*} innerContent inner html content of grid box
- * @returns a div grid box element
+ * function that creates a div cell as a dom element
+ * @param {*} innerContent inner html content of the div cell, wrapped by a span tag
+ * @returns a div cell element
  */
-function createGridBoxElement (innerContent) {
-	const gridBox = document.createElement('div');
-	gridBox.classList.add('cell');
-	gridBox.addEventListener('click', function(){
-		this.classList.toggle('clicked');
-	})
-	gridBox.innerHTML = `<span>${innerContent}</span>`
-	return gridBox;
+function createGridCellElement (innerContent, cellsPerRow) {
+	const gridCell = document.createElement('div');
+	gridCell.classList.add('cell');
+	gridCell.style.width = `calc(100% / ${cellsPerRow}`;
+	gridCell.style.height = gridCell.style.width;
+	gridCell.innerHTML = `
+	<span>${innerContent}</span>`
+	return gridCell;
+}
+
+
+/**
+ * function that generates a game board
+ * @returns a game board based on difficulty level selected
+ */
+function generateGame(){
+	let cellsNumber;
+	let cellsPerRow;
+	switch(parseInt(levelSelectElement.value)){
+		case 1 :
+			cellsNumber = 100;
+			break;
+		case 2 :
+			cellsNumber = 81;
+			break;
+		case 3 :
+			cellsNumber = 49;
+			break;
+		default :
+		scoreOutput.innerHTML ='Please select a level, then press play';
+	}
+	cellsPerRow = Math.sqrt(cellsNumber);
+	for ( let i = 1; i <= cellsNumber; i++){
+		const currentGridCell = createGridCellElement(i, cellsPerRow);
+		currentGridCell.addEventListener('click', function() {
+			this.classList.add('clicked');
+		})
+		grid.appendChild(currentGridCell);
+	}
+	return grid;
 }
